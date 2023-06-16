c417da2ce0064d62f0e0a165b43c27febccd36d1
aj-badr <badreddine.ajlil@gmail.com>
2023-06-16 08:51:49 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,14 +7,15 @@
 
 int main(void)
 {
-       	int i, j;
+       	int i;
+	int j;
 	
 for (i = 0; i <= 9; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
 	putchar(i + '0');
-	putchar(j + '0);
+	putchar(j + '0');
        	if (i < 8 || j < 9)
 	{
 	putchar(',');
